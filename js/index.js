610cb522dbdba8f9bad8ea83041214ee228e3867
aLLeNxD <240831608@qq.com>
2017-08-05 15:45:48 +0800

    upload files

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 $(function () {
-  $('#navHidden').on('click', function () {
-    $('#navBox').hide();
-  });
+	/* 点击“登录”按钮 */
+	$(".enter-button").on('click', function(event) {
+		event.preventDefault();
+		/* 这里放一个判断语句，用来判断用户名和密码是否输入正确 */
+
+		/* 如果输入不正确，则显示错误信息 */
+		$(".warning-info").show();
+	});
 });
